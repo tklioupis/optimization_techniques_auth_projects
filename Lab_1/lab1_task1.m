da2a0c5b7d627ce1af60411cca59877630e16f8d
Theodoros Lioupis <tklioupis@gmail.com>
2022-11-12 18:25:43 +0200

    updated task1

--- Lab_1/lab1_task1.m
+++ Lab_1/lab1_task1.m
@@ -10,19 +10,20 @@ a1 = -1;
 b1 = 3;
 
 for case_of_fxi = 1:3 %loop that runs 3 times, each time for one of the three functions
-    M = 1000; %length of the different lamdas or epsilons (depending on which is changeable)
+    M1 = 1000; %length of the different lamdas or epsilons (depending on which is changeable)
     %first case: l = 0.01 (const) and e changeable
-    l_C1 = 0.01 * ones(1,M);
-    e_c1 = linspace(0.00004,0.004,M);
-    [k_c1,a_c1,b_c1,calcs_c1] = bisector_method(case_of_fxi,l_C1,e_c1,a1,b1,M);
+    l_c1 = 0.01 * ones(1,M1);
+    e_c1 = linspace(0.00004,0.004,M1);
+    [k_c1,a_c1,b_c1,calcs_c1] = bisector_method(case_of_fxi,l_c1,e_c1,a1,b1,M1);
     %second case: l changeable and e = 0.001 (const)
-    l_c2 = linspace(0.005,0.05,M); 
-    e_c2 = 0.001 * ones(1,M);
-    [k_c2,a_c2,b_c2,calcs_c2] = bisector_method(case_of_fxi,l_c2,e_c2,a1,b1,M);
+    l_c2 = linspace(0.005,0.05,M1); 
+    e_c2 = 0.001 * ones(1,M1);
+    [k_c2,a_c2,b_c2,calcs_c2] = bisector_method(case_of_fxi,l_c2,e_c2,a1,b1,M1);
     %third case: l = 0.01, e = 0.001
     l_c3 = 0.01;
     e_c3 = 0.001;
-    [k_c3,a_c3,b_c3,calcs_c3] = bisector_method(case_of_fxi,l_c3,e_c3,a1,b1,1);
+    M2 = 1;
+    [k_c3,a_c3,b_c3,calcs_c3] = bisector_method(case_of_fxi,l_c3,e_c3,a1,b1,M2);
 
     %plots of the results for each case of study
     %first case: plot the calcualtions of fxi for l = 0.01 (const) and e changeable
@@ -69,25 +70,25 @@ b(1)=b1; %I dont knpw the size of b so I will use dynamic memory allocation
 k = ones(1,M); % k is of size 1XM. M is the length of the different lamdas or epsilons 
 calcs = zeros(1,M); %to store every fxi calculation
 i = 1;
-    while (b(k(i)) - a(k(i))) >= l(i) %the loop terminates when I have checked that bk-ak < l for all possible lamdas (const or changeable)
-        x1(k(i)) = (a(k(i))+b(k(i)))/2 - e(i);  %e can be const or changeable
-        x2(k(i)) = (a(k(i))+b(k(i)))/2 + e(i);  %e can be const or changeable
-        y1 = f(x1(k(i)),case_of_fxi);
-        y2 = f(x2(k(i)),case_of_fxi);
-        calcs(i) = calcs(i) + 2; %each time I calculate the value of fxi, I increase reps(i)
-        if (y1 < y2) 
-            a(k(i)+1) = a(k(i));
-            b(k(i)+1) = x2(k(i));
-        else 
-            a(k(i)+1) = x1(k(i));
-            b(k(i)+1) = b(k(i));
-        end
-        k(i) = k(i)+1;
-        if ((b(k(i)) - a(k(i)) < l(i)) && (i < M)) %if bk-ak<l and I havent checked all lamda cases
-            %when I have found the [ak,bk] with the x* for a spesific lamda, I increase i so I can continue with the next lamda
-            i = i + 1;
-        end    
+while (b(k(i)) - a(k(i))) >= l(i) %the loop terminates when I have checked that bk-ak < l for all possible lamdas (const or changeable)
+    x1(k(i)) = (a(k(i))+b(k(i)))/2 - e(i);  %e can be const or changeable
+    x2(k(i)) = (a(k(i))+b(k(i)))/2 + e(i);  %e can be const or changeable
+    y1 = f(x1(k(i)),case_of_fxi);
+    y2 = f(x2(k(i)),case_of_fxi);
+    calcs(i) = calcs(i) + 2; %each time I calculate the value of fxi, I increase reps(i)
+    if (y1 < y2) 
+        a(k(i)+1) = a(k(i));
+        b(k(i)+1) = x2(k(i));
+    else 
+        a(k(i)+1) = x1(k(i));
+        b(k(i)+1) = b(k(i));
     end
+    k(i) = k(i)+1;
+    if ((b(k(i)) - a(k(i)) < l(i)) && (i < M)) %if bk-ak<l and I havent checked all lamda cases
+        %when I have found the [ak,bk] with the x* for a spesific lamda, I increase i so I can continue with the next lamda
+        i = i + 1;
+    end    
+end
 end
     
 
